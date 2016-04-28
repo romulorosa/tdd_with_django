c09a7467ce29598922b77b6cd330710e58f9f70e
Romulo Rosa Furtado <romuloros@gmail.com>
2016-04-28 16:06:57 -0300

    Extend functional test with input and table

--- functional_test.py
+++ functional_test.py
@@ -8,9 +8,26 @@ class HomePageTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_home_page(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do', header.text)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys("\n")
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        #self.fail('Finish the test!')
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(warnings='ignore')
