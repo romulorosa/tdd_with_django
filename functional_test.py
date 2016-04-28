8cad0cf2071ac14dd0271f2f3a5da1b202fbf30c
Romulo Rosa Furtado <romuloros@gmail.com>
2016-04-28 17:24:06 -0300

    Change find function

--- functional_test.py
+++ functional_test.py
@@ -23,9 +23,10 @@ class HomePageTest(unittest.TestCase):
         inputbox.send_keys("\n")
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: Buy peacock feathers',
+            [row.text for row in rows]
         )
 
         #self.fail('Finish the test!')
